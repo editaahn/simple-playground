8fd7083b37d1b3bd782951dc7f9b8906b33d9438
Rita <41330862+editaahn@users.noreply.github.com>
2023-08-14 21:48:24 +0900

    feat: set react-query and create error handler (#4)
    * config: react-query
    
    * feat: create request function
    
    * chore: create alias for directories
    
    * feat: suspense & error handling
    
    * chore: separate a next change

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,20 @@
-import { BrowserRouter, Routes, Route } from '../node_modules/react-router-dom/dist/index';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { PianoPage } from './pages/piano';
 
 export function App() {
+  const queryClient = new QueryClient();
+
   return (
-    <BrowserRouter>
-      {/* <Header /> */}
-      <Routes>
-        {/* <Route path="/" element={<Main />}></Route> */}
-        <Route path="/piano" element={<PianoPage />}></Route>
-        {/* <Route path="*" element={<NotFound />}></Route> */}
-      </Routes>
-    </BrowserRouter>
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        {/* <Header /> */}
+        <Routes>
+          {/* <Route path="/" element={<Main />}></Route> */}
+          <Route path="/piano" element={<PianoPage />}></Route>
+          {/* <Route path="*" element={<NotFound />}></Route> */}
+        </Routes>
+      </BrowserRouter>
+    </QueryClientProvider>
   );
 }
---
+++ src/utils/index.ts
@@ -0,0 +1,4 @@
+export { binder } from './binder';
+export { request } from './request';
+export { ApiErrorBoundary } from './ApiErrorBoundary';
+export { ErrorAlert } from './ErrorAlert';
