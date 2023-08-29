e2aeace0bb7e9a2d294c8e4421af35d3c9adf08e
Rita <editaahn@yahoo.com>
2023-08-29 15:21:01 +0900

    feat: develop scroll animation component

--- src/libs/index.ts
+++ src/libs/index.ts
@@ -2,3 +2,4 @@ export { Piano } from './piano';
 export { Albums } from './albums';
 export { EmployeeChoiceSubmitButton, EmployeeList, SelectedEmployeeList, useEmployeeChoice } from './employees';
 export * from './categories';
+export { ScrollImageAnimation } from './scrollImageAnimation';
